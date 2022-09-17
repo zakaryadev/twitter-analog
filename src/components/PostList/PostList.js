ac2d5539cdd347fbc4264f9d1e10ed9c4e3655d6
Zakarya <zakaryamuratbaev13@gmail.com>
2022-09-17 16:27:55 +0500

    A

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -1,13 +1,18 @@
 import PostListItem from "../PostListItem";
 import "./PostList.css";
 
-const PostList = ({ posts, isDelete }) => {
+const PostList = ({ posts, isDelete, onToggleImportant, onToggleLike }) => {
   return (
     <ul className="app-list list-group">
       {posts.map((item) => {
         const { id, ...itemProps } = item;
         return (
-          <PostListItem key={id} {...itemProps} isDelete={() => isDelete(id)} />
+          <PostListItem
+            key={id} {...itemProps}
+            isDelete={() => isDelete(id)}
+            onToggleImportant={() => onToggleImportant(id)}
+            onToggleLike={() => onToggleLike(id)}
+          />
         );
       })}
     </ul>
